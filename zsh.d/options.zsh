6725a6df09e8a3460d028dd3b5e88c613b60c3ac
Logan Attwood <logan@jnickel.com>
2015-11-03 13:48:24 -0400

    move vim stuff into separate file

--- zsh.d/options.zsh
+++ zsh.d/options.zsh
@@ -1,11 +1,4 @@
 unsetopt beep
-export EDITOR=vim
-bindkey -v
-# removes esc-/ chord
-bindkey -r "^[/"
-# ctrl-r starts searching history backward
-bindkey '^r' history-incremental-search-backward
-
 export GOPATH=$HOME/gopath
 export PATH=/usr/local/sbin:$HOME/bin:$GOPATH/bin:$PATH:.
 export ANDROID_HOME=/usr/local/opt/android-sdk
---
+++ zsh.d/vim.zsh
@@ -0,0 +1,8 @@
+export EDITOR=vim
+# vim shell keybindings
+bindkey -v
+# removes esc-/ chord
+bindkey -r "^[/"
+# ctrl-r starts searching history backward
+bindkey '^r' history-incremental-search-backward
+
